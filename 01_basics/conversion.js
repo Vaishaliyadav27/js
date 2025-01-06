7479f934a00a41a927a09d3c34c051f16ba3d311
Vaishaliyadav27 <vaishaliyadav308@gmail.com>
2025-01-06 17:28:29 +0530

    Create conversion.js

---
+++ 01_basics/conversion.js
@@ -0,0 +1,15 @@
+let score=33
+console.log(typeof score);
+// output number
+
+let score="33"
+console.log(typeof score)
+// output string
+
+let score=null
+console.log(typeof score)
+// output 0
+
+let valueInNumber=Number(score)
+console.log(typeof valueINumber);
+// output number
