f5acc7b649e43c9dcdc630f7e6f346aefae259f8
Vaishaliyadav27 <vaishaliyadav308@gmail.com>
2025-01-06 11:01:29 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,14 @@
 const accountId=123456
 let accountEmail="vaishaliyadav@234"
 var accountPassword="1234"
-accountCity=jaipur
+let accountCity=jaipur
+let accountState;
 
 accountId=2
+accountEmail="vaishali123@gamil.com"
+accountCity="hydrabad"
+
 
 console.log(accountId);
+console.table([accountId,accountEmail,accountEmail,accountPassword,accountCity,accountState])
 
