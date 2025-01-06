21fbef96c11025c79aa07ad393cb9c219f5eacdd
Vaishaliyadav27 <vaishaliyadav308@gmail.com>
2025-01-06 12:57:51 +0530

    Create datatypes.js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+// "use strict" 
+// use scrict treat all js code as newer version
+alert("3+3")
+// alert is not defined
+
+console.log("hi")
+console.log("3+3")
+// main thing is code readable
+
+let name="vaishali"
+let age=22
+let isloggedin =false
+
+// datatypes
+
+// number=2 to power 54
+// bigit=(for big numbers)
+// boolean=true/false
+// string=
+// null=standalone value
+// undefined
+// symbol=unique
+// object
+console.log(typeof "vaishali")
+// output is string
+console.log(typeof "null")
+// object
+console.log(typeof "undefined")
+// undefined
