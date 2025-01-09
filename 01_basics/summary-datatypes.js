0875a95432c59d161fd4f72e5ccb4947449cf417
Vaishaliyadav27 <vaishaliyadav308@gmail.com>
2025-01-09 13:17:07 +0530

    Update summary-datatypes.js

--- 01_basics/summary-datatypes.js
+++ 01_basics/summary-datatypes.js
@@ -3,3 +3,25 @@
 
 // reference(non primitive)
 // array function object
+
+const id =symbol('123')
+cons newid=symbol('123')
+console.log(id===newid);
+// output false
+
+const score=100
+const scorevalue=100.3
+cosole.log(typeof score)
+// number
+
+const myfunction=function();
+console.log(typeof myfunction)
+// function
+
+const outsideTemp=false
+console.log(typeof outsideTemp)
+// object
+
+const heros=["saktiman","cvb","ttyu"]
+console.log(typeof heros)
+// object
